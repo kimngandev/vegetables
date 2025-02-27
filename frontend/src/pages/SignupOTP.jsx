5843460c229545f8901cb10b84db1a4d09431edc
kimdev <kimngan.stack@gmail.com>
2025-02-27 11:19:10 +0700

    fixbug

--- frontend/src/pages/SignupOTP.jsx
+++ frontend/src/pages/SignupOTP.jsx
@@ -7,7 +7,7 @@ import { toast } from 'react-toastify'
 
 const SignupOTP = () => {
 
-<<<<<<< HEAD
+
  
   
   const handleKeyDown = (e, index) => {
@@ -16,8 +16,7 @@ const SignupOTP = () => {
     }
   }
 
-=======
->>>>>>> bcce4f1da61a308bcf243ee4bcbd9777b8b3fc7b
+
   const [values, setValues] = useState(["", "", "", ""]);
   const inputRefs = useRef([]);
 
